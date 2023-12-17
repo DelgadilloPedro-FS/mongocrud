b60ba56b7a9a36563afc7740c3f2efadd901d610
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2023-12-17 16:33:14 -0600

    fixed controller functions o add validaiton for cats already submitted

--- app/controllers/catController.js
+++ app/controllers/catController.js
@@ -1,7 +1,7 @@
-const Cats = require("../models/Cat");
+const Cat = require("../models/Cat");
 
 const getAllCats = async (req, res) => {
-  const allCats = await Cats.find(); // Find all cats
+  const allCats = await Cat.find(); // Find all cats
   res.status(200).json({
     success: true,
     message: `${req.method} - request to Cat endpoint`,
@@ -11,7 +11,7 @@ const getAllCats = async (req, res) => {
 
 const getCatById = async (req, res) => {
   const { id } = req.params;
-  const cat = await Cats.findById(id); // Find cat by ID
+  const cat = await Cat.findById(id); // Find cat by ID
   if (!cat) {
     return res.status(404).json({
       success: false,
@@ -27,19 +27,35 @@ const getCatById = async (req, res) => {
 
 const createCat = async (req, res) => {
   const { cat } = req.body;
-  const newCat = await Cats.create(cat);
-  console.log("data >>>>", newCat);
-  res.status(200).json({
-    success: true,
-    message: `${req.method} - request to Cat endpoint`,
-  });
+  Cat.find({ name: cat.name, age: cat.age })
+    .exec()
+    .then((result) => {
+      if (result.length > 0) {
+        return res.status(406).json({
+          message: "Cat already in shelter",
+        });
+      }
+      const newCat = Cat.create(cat);
+      console.log("data >>>>", newCat);
+      res.status(200).json({
+        success: true,
+        message: `${req.method} - Cat created successfully`,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        error: {
+          message: "could not save cat",
+        },
+      });
+    });
 };
-
 
 const updateCat = async (req, res) => {
   const { id } = req.params;
   const updateData = req.body.cat; // Get updated data
-  const updatedCat = await Cats.findByIdAndUpdate(id, updateData, { new: true }); // Find and update cat
+  const updatedCat = await Cat.findByIdAndUpdate(id, updateData, { new: true }); // Find and update cat
   if (!updatedCat) {
     return res.status(404).json({
       success: false,
@@ -59,13 +75,13 @@ const deleteCat = async (req, res) => {
   if (!deletedCat) {
     return res.status(404).json({
       success: false,
-      message: `Cat with ID ${id} not found`
+      message: `Cat with ID ${id} not found`,
     });
   }
   res.status(200).json({
     success: true,
     message: `${req.method} - request to Cat endpoint`,
-    deletedCat
+    deletedCat,
   });
 };
 
